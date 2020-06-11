1f5f59e45ccd56f8a5c4fa37d5b6357963410d34
Chuckz Okoye <tricelex@gmail.com>
2020-06-11 19:40:58 +0100

    created homepage on the portfolio app

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
