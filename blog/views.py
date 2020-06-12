ed962c745e1ca22664e924d6f7420493888878f8
Chuckz Okoye <tricelex@gmail.com>
2020-06-12 09:11:11 +0100

    added blog objects and display them on blog page

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from blog.models import Blog
+
+
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.order_by('-date')[:3]
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
