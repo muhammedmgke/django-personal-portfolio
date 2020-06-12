0d49170c1f69de832567a71ed3194abf38e2eaa3
Chuckz Okoye <tricelex@gmail.com>
2020-06-12 02:07:31 +0100

    routing for blog and blog home view

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -21,7 +21,8 @@ from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('blog/', include('blog.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
