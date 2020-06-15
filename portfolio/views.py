231b54325d70c9c69efcc6a2d6371ef0f53b2af9
Chuckz Okoye <tricelex@gmail.com>
2020-06-15 13:10:50 +0100

    added bootstrap template and redesigned homepage

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Project
+from blog.models import Blog
 
 
 # Create your views here.
 def home(request):
     projects = Project.objects.all()
-    return render(request, 'portfolio/home.html', {'projects': projects})
+    blogs = Blog.objects.order_by('-date')
+    return render(request, 'portfolio/home.html', {'projects': projects, 'blogs': blogs})
+
+
+def all_blogs(request):
+    blogs = Blog.objects.order_by('-date')
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
